268de885fd16066214b9d1a7109ae1034fe79ae8
avilas <avilas@mit.edu>
2020-01-24 15:37:01 -0500

    API compatability added

--- OneDrive/Desktop/homepage/projectploutos-android-main/ProjectPloutos/app/src/main/java/com/example/projectploutos/MySingleton.java
+++ OneDrive/Desktop/homepage/projectploutos-android-main/ProjectPloutos/app/src/main/java/com/example/projectploutos/MySingleton.java
@@ -12,28 +12,9 @@ import com.android.volley.toolbox.Volley;
 public class MySingleton {
     private static MySingleton instance;
     private RequestQueue requestQueue;
-    private ImageLoader imageLoader;
-    private static Context ctx;
 
     private MySingleton(Context context) {
-        ctx = context;
-        requestQueue = getRequestQueue();
-
-        imageLoader = new ImageLoader(requestQueue,
-                new ImageLoader.ImageCache() {
-                    private final LruCache<String, Bitmap>
-                            cache = new LruCache<String, Bitmap>(20);
-
-                    @Override
-                    public Bitmap getBitmap(String url) {
-                        return cache.get(url);
-                    }
-
-                    @Override
-                    public void putBitmap(String url, Bitmap bitmap) {
-                        cache.put(url, bitmap);
-                    }
-                });
+        requestQueue = Volley.newRequestQueue(context.getApplicationContext());
     }
 
     public static synchronized MySingleton getInstance(Context context) {
@@ -44,19 +25,6 @@ public class MySingleton {
     }
 
     public RequestQueue getRequestQueue() {
-        if (requestQueue == null) {
-            // getApplicationContext() is key, it keeps you from leaking the
-            // Activity or BroadcastReceiver if someone passes one in.
-            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
-        }
         return requestQueue;
-    }
-
-    public <T> void addToRequestQueue(Request<T> req) {
-        getRequestQueue().add(req);
-    }
-
-    public ImageLoader getImageLoader() {
-        return imageLoader;
     }
 }
--- OneDrive/Desktop/homepage/projectploutos-android-main/ProjectPloutos/app/src/main/java/com/example/projectploutos/Test.java
+++ OneDrive/Desktop/homepage/projectploutos-android-main/ProjectPloutos/app/src/main/java/com/example/projectploutos/Test.java
@@ -5,8 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Random;
+
+import static android.app.PendingIntent.getActivity;
 
 public class Test extends AppCompatActivity {
+    private RequestQueue mQueue;
+    private TextView mTextViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +40,33 @@ public class Test extends AppCompatActivity {
         String accountType = intent.getStringExtra("account");
         TextView textView3 = findViewById(R.id.textView3);
         textView3.setText(accountType);
+        mTextViewResult = findViewById(R.id.textView4);
+        mQueue = MySingleton.getInstance(this).getRequestQueue();
+
+        Random rnd = new Random();
+        String url = "https://superheroapi.com/api/2577189005834916/" + String.valueOf(rnd.nextInt(731) + 1);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String name = response.getString("name");
+                            String real = response.getJSONObject("biography").getString("full-name");
+                            String birthplace = response.getJSONObject("biography").getString("place-of-birth");
+                            mTextViewResult.setText("\n" + name + ", " + real + ", " + birthplace + "\n");
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
     }
 }
